35ea9ba90d69d336e4856c9d6f377d0401807a9a
Elielton Kremer <elielton@integra.do>
2020-11-27 12:49:19 -0300

    fix server command parameter

--- logos_server/command.py
+++ logos_server/command.py
@@ -4,12 +4,14 @@ from logos.context import context
 
 class ServeCommand(AbstractCommand):
 
+    def __init__(self, server):
+        self.server = server
+
     def define_arguments(self):
         self.argument_parser.add_argument('--host', default='localhost')
         self.argument_parser.add_argument('--port', default=8080, type=int)
 
     def execute(self):
-        app = context.get('app.server')
         arguments = self.arguments
         app.run(
             host=arguments.host,
